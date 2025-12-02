5a02604eb611cc6e4728506d5b1e496da253c85d
Marvin <mvt5393@yahoo.com>
2025-12-02 18:25:50 -0500

    Checks if the two words are Anagram or not, using dictionary method

---
+++ Anagram Check.py
@@ -0,0 +1,28 @@
+def is_anagram(text1, text2):
+    """
+    This function checks if the following two text1 & text2 are anagrams.
+    
+    :param text1: String input
+    :param text2: String input
+    """
+
+    text1_clean = text1.replace(" ", "")
+    text2_clean = text2.replace(" ", "")
+
+    if len(text1_clean) != len(text2_clean):
+        return False
+
+    freq = {}
+
+    for char in text1_clean:
+        freq[char] = freq.get(char, 0) + 1
+    for char in text2_clean:
+        freq[char] = freq.get(char, 0) - 1
+    for val in freq.values():
+        if val != 0:
+            print(f"--> The words {text1} & {text2} are not Anagram.")
+            return False
+    print(f"--> The words {text1} & {text2} are Anagram")
+    return True
+
+is_anagram("Hello", "World")
