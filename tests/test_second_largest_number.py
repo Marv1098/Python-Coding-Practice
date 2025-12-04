fcb69fd3c7eeafba04c5e65d818d57f9945eb578
Marvin <mvt5393@yahoo.com>
2025-12-03 19:52:12 -0500

    Created a tests folder for readability and test each file in source folder

---
+++ tests/test_second_largest_number.py
@@ -0,0 +1,31 @@
+import pytest
+
+from second_largest_number import second_largest
+
+@pytest.mark.parametrize(
+    "input_list, expected",
+    [
+        ([150, 10, 30, 20, 15], 30),        # Positive Numbers
+        ([-100, -10, -35, -50, -5], -10),   # Negative Numbers
+        ([150, -100, 35, 90, -15], 90),     # Mixed Numbers
+        ([3, 7], 3)                         # Two Elements
+    ]
+)
+def test_second_largest_values(input_list, expected):
+    assert second_largest(input_list) == expected
+
+def test_empty_list_raises_exception():
+    with pytest.raises(ValueError):
+        second_largest([])
+
+def test_single_element_raises_exception():
+    with pytest.raises(ValueError):
+        second_largest([42])
+
+def test_two_element_duplicates_raises_exception():
+    with pytest.raises(ValueError):
+        second_largest([42, 42])
+
+def test_all_same_elements_raises_exception():
+    with pytest.raises(ValueError):
+        second_largest([42, 42, 42])
---
+++ tests/test_smallest_number.py
@@ -0,0 +1,20 @@
+import pytest
+
+from smallest_number import smallest
+
+@pytest.mark.parametrize(
+    "input_list, expected",
+    [
+        ([5,2,9,1,7], 1),       # Positive Numbers
+        ([-3,-7,-1,-9], -9),    # Negative Numbers
+        ([3,-2,7,0,-5], -5),    # Mixed Numbers
+        ([2,2,3,1,1], 1),       # Duplicates
+        ([42], 42)              # Single Element
+    ]
+)
+def test_smallest_values(input_list, expected):
+    assert smallest(input_list) == expected
+
+def test_empty_list_raises_exception():
+    with pytest.raises(ValueError):
+        smallest([])
