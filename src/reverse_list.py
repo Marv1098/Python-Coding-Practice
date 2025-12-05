20ec15d5a0b9b0acf9ebaef1e125c73e733ad865
Marvin <mvt5393@yahoo.com>
2025-12-04 19:19:43 -0500

    Fixed the code to raise exception for input None.

--- src/reverse_list.py
+++ src/reverse_list.py
@@ -1,12 +1,17 @@
-def reverse_list(nums_list):
+def reverse_list(input_list):
     """
     Reverse a given numbers list without using built in reverse()
     """
+    # Empty list validation
+    if len(input_list) == 0:
+        raise ValueError("Cannot reverse a empty list.")
+    # Input type validation
+    if not isinstance(input_list, list):
+        raise TypeError("Input must be a list.")
+    
     reversed_list = []
-    for num in nums_list[::-1]:
-        reversed_list.append(num)
+    for item in range(len(input_list)-1, -1, -1):
+        reversed_list.append(input_list[item])
 
-    print("--> List in reverse order: ",reversed_list)
+    print("--> List in reverse order: ", reversed_list)
     return reversed_list
-
-reverse_list([2,3,20,30,45,100,150])
