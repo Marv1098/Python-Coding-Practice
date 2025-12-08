ce19510999de04713d46faaeb5a8141f35ad8145
Marvin <mvt5393@yahoo.com>
2025-12-07 21:31:17 -0500

    Updated the code to handle edge cases.

--- src/min_and_max_number.py
+++ src/min_and_max_number.py
@@ -2,7 +2,18 @@ def find_min_and_max(nums_list):
     """
     The functions finds both Min and Max in the numbers list.
     """
-
+    # Input Validation
+    if not isinstance(nums_list, list):
+        raise TypeError("Input must be a numeric list.")
+    
+    # Empty List Validation
+    if len(nums_list) == 0:
+        raise ValueError("List cannot be empty")
+    
+    # Non-numeric Validation
+    if not all(isinstance(num, (int, float)) for num in nums_list):
+        raise TypeError("All elements in the list must be numeric.")
+    
     min_num = nums_list[0]
     max_num = nums_list[0]
     for nums in nums_list:
--- src/two_sum.py
+++ src/two_sum.py
@@ -1,30 +1,44 @@
-def findTwoSum(nums, target):
+def findTwoSum(nums_list, target):
     """
     Check the list if there are two numbers that add up to the target value.
     
     nums: Input of Number List
     target: Input Number value
     """
-
+    # Input Validation:
+    if not isinstance(nums_list, list):
+        raise TypeError("Input must be a numeric list")
+    
+    # Empty List Validation
+    if len(nums_list) == 0:
+        raise ValueError("Cannot find sum from empty list.")
+    
+    # Non-numeric Validation
+    if not all(isinstance(num, (int, float)) for num in nums_list):
+        raise TypeError("Input has to be numeric values.")
+    
     # Dictionary that maps number to its index in the list
     numsDict = {}
+    precision = 10
 
     # Loop through the list using both index (i) and value (num)
-    for i, num in enumerate(nums):
+    for i in range(len(nums_list)):
+        num = nums_list[i]
 
         # Compute the number we need to get the target
-        diff = target - num
+        num_rounded = round(num, precision)
+        diff_rounded = round(target - num, precision)
 
         # Check if the number we need has already been seen earlier in the list
-        if diff in numsDict:
-            print(f"The indices that add up to the value {target} are: {[numsDict[diff], i]}")
-            return [numsDict[diff], i]
+        if diff_rounded in numsDict:
+            print(f"The indices that add up to the value {target} are: {[numsDict[diff_rounded], i]}")
+            return [numsDict[diff_rounded], i]
         
         # If not found yet, store the current number and its index
-        numsDict[num] = i
+        numsDict[num_rounded] = i
     
     # If the loop ends with no match, there's is no valid pair
     print(f"There are no indices that add up to the target value {target}")
     return None
 
-findTwoSum([1,3,7,9,2], 11)
+findTwoSum([1.1, 2.2, 3.3], 3.3)
