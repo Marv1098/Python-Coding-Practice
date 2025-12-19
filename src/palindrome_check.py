c24dd1996fe32c368a2d8959e7808c5332d2c27e
Marvin <mvt5393@yahoo.com>
2025-12-18 20:21:00 -0500

    Updated code to handle edge cases.

--- src/palindrome_check.py
+++ src/palindrome_check.py
@@ -2,17 +2,31 @@ def is_palindrome(text):
    """
    This function check if a given input is a palindrome or not
    """
-   text = text.lower()
+   
+   # Type Validation
+   if not isinstance(text, (str, int)):
+       raise TypeError("Input must be a string or integer.")
+   
+   # convert int to string
+   text = str(text).lower()
+
+   # Remove non-alphanumeric characters (space, punctuation, symbols)
+   clean_text = ''.join(c for c in text if c.isalnum())
+
+   # Empty input validation
+   if not clean_text:
+       raise ValueError("Input cannot be empty.")
+
    left = 0
-   right = len(text) - 1
+   right = len(clean_text) - 1
    while left < right:
-       if text[left] != text[right]:
-           print(f"--> The word {text} is not a Palindrome")
+       if clean_text[left] != clean_text[right]:
+           print(f"--> The input {text} is not a Palindrome")
            return False
        else:
            left += 1
            right -= 1
-   print(f"--> The word {text} is a Palindrome")
+   print(f"--> The input {text} is a Palindrome")
    return True
 
-is_palindrome("RaceCar")
+is_palindrome(" ")
